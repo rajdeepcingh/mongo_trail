e670375921247300943d92014dc5e92834dbbf60
Singh <rajdeep.singh@accenture.com>
2020-07-30 17:24:15 +0530

    first commit123

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,6 +1,5 @@
 package com.example.demo.model;
 
-
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,13 +7,8 @@ import java.util.Map;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @Document
-@Getter
-@Setter
-class User {
+public class User {
 
 	@Id
 	private String userId;
@@ -22,5 +16,36 @@ class User {
 	private Date creationDate = new Date();
 	private Map<String, String> userSettings = new HashMap<>();
 
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Map<String, String> getUserSettings() {
+		return userSettings;
+	}
+
+	public void setUserSettings(Map<String, String> userSettings) {
+		this.userSettings = userSettings;
+	}
 }
 
